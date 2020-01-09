57dd717af7876ad32d339ee20122c4bdb9962f96
tyh <756467256@qq.com>
2020-01-09 09:29:09 +0800

    {YunHan}{feat}--{ProjectManager}:finish ajax

--- projectManager/ajax.js
+++ projectManager/ajax.js
@@ -17,8 +17,7 @@ window.ajax = function (options) {
   //2.监听onreadystatechange事件
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
-        if (xhr.status >= 200 && xhr.status < 300 ||
-            xhr.status === 304){
+        if (xhr.status == 200 || xhr.status == 201){
           options.success(JSON.parse(xhr.responseText));
         }
         else {
@@ -31,21 +30,16 @@ window.ajax = function (options) {
   //4.设置请求头部
   //5.调用send()方法
   switch (options.method) {
-    case "POST":
+    case "POST": 
+    case "PUT":
       xhr.setRequestHeader("Content-type","application/json");
       xhr.send(JSON.stringify(options.data));
       break;
-    case "GET":
-      xhr.send(null);  
-      break;
-    case "PUT": 
-      xhr.setRequestHeader("Content-type","application/json");
-      xhr.send(JSON.stringify(options.data));
-      break;
-    case "DELETE":
+    case "GET": 
+    case "DELETE":  
       xhr.send(null);  
       break;
     default:
-      break;  
+      console.log("不支持的请求方法")  
   }
 }
