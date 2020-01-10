22597740f7c9623a680fbe5944adfc9539b24c9a
tyh <756467256@qq.com>
2020-01-10 15:21:06 +0800

    {YunHan}{fixed}--{ProjectManager}:add delete page

--- projectManager/index.js
+++ projectManager/index.js
@@ -1,28 +1,108 @@
-(function () {
+//(function () {
   const API_ROOT = "http://localhost:3000/projects";
-  let $projectName = document.getElementById('project-name');
-  let $projectDes = document.getElementById('project-des');
-  let $endTime = document.getElementById('end-time');
-  let $projectStatus = document.getElementById('project-status');
-  let $projectDelete = document.getElementById('project-delete');
+  let $projectTable = document.getElementById('project-table');
+  let $taskingNumber = document.getElementsByClassName('tasking-number');
+  let $taskingPercent = document.getElementsByClassName('tasking-percent');
+  let deleteId = 0;
 
-  // TODO： get user list，when success, run renderUserList
   function getListData() {
     var options = {
       url: API_ROOT,
       header: {},
       method: "GET",
       success: function(res) {
-        renderProjectList(res); 
+        initialProjectList(res); 
       },
       error: function (error) {
-          console.log('error', error);
+        console.log('error', error);
       }
     }
     ajax(options);
   }
 
-  function renderProjectList(data) {
-    console.log(data);
+  function deleteItemData(id) {
+    var options = {
+      url: API_ROOT +'/'+ id,
+      header: {},
+      method: "DELETE",
+      success: function(res) {
+        deleteItem(res); 
+      },
+      error: function (error) {
+        console.log('error', error);
+      }
+    }
+    ajax(options);
   }
-})()
+
+  function initialProjectList(data) {
+    if (Array.isArray(data) && !data instanceof Array) {
+      return false;
+    }
+    $projectTable.innerHTML = data.reduce((acc, cur) => {
+      return acc += `<tr data-id="${cur.id}">
+                       <td>${cur.name}</td>
+                       <td title="${cur.description}">
+                         <span class="hidden-over-two-row">${cur.description}</span>
+                       </td>
+                       <td>${cur.endTime}</td>
+                       <td class="${cur.status}">${cur.status}</td>
+                       <td><button class="delete">删除</button></td>
+                     </tr>`;
+    }, '');
+    projectCount(data);
+  }
+
+  function projectCount(data) {
+    data.forEach(element => {
+      switch(element.status) {
+        case "ACTIVE":
+          $taskingNumber[1].innerHTML++; 
+          break;    
+        case "PENDING":
+          $taskingNumber[2].innerHTML++;
+          break;
+        case "CLOSED":
+          $taskingNumber[3].innerHTML++;
+          break;
+        default:
+          console.warn("不存在的状态")
+      }
+    });
+    $taskingNumber[0].innerHTML = data.length;
+  
+    Array.from($taskingPercent).forEach((element, index) => element.innerHTML
+                                         = parseInt($taskingNumber[index + 1].innerHTML / $taskingNumber[0].innerHTML * 100) + "%");
+  }
+
+  function deleteMask(id) {
+    document.getElementById("delete-mask").style.display = "block";
+    document.getElementsByTagName("body")[0].className = "no-move";
+    deleteId = id; 
+  }
+
+  function deleteItem(id) {
+    document.getElementById("delete-mask").style.display = "none";
+    // var $item = $projectTable.querySelector(`tr[data-id="${id}"]`);
+    // $projectTable.removeChild($projectTable.querySelector(`tr[data-id='${id}']`));
+  } 
+  
+  getListData();
+  document.addEventListener("click", function(e) {
+    switch(e.target.className) {
+      case "delete":
+        let id = e.target.parentNode.parentNode.getAttribute('data-id');
+        deleteMask(id);
+        break;
+      case "back iconfont":
+      case "cancel":  
+        document.getElementById("delete-mask").style.display = "none";
+        break;
+      case "comfirm":
+        deleteItemData(deleteId);
+        break;  
+      default:
+        console.log(e.target.className)  
+    }
+  })
+//})()
