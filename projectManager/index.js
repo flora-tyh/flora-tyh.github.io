478e631962fcb6ef51baa9abc802a7801a59ed04
tyh <756467256@qq.com>
2020-01-10 20:45:18 +0800

    {YunHan}{fixed}--{ProjectManager}:delete page list

--- projectManager/index.js
+++ projectManager/index.js
@@ -1,4 +1,4 @@
-//(function () {
+(function () {
   const API_ROOT = "http://localhost:3000/projects";
   let $projectTable = document.getElementById('project-table');
   let $taskingNumber = document.getElementsByClassName('tasking-number');
@@ -25,8 +25,8 @@
       url: API_ROOT +'/'+ id,
       header: {},
       method: "DELETE",
-      success: function(res) {
-        deleteItem(res); 
+      success: function() {
+        deleteItem(id); 
       },
       error: function (error) {
         console.log('error', error);
@@ -46,7 +46,7 @@
                          <span class="hidden-over-two-row">${cur.description}</span>
                        </td>
                        <td>${cur.endTime}</td>
-                       <td class="${cur.status}">${cur.status}</td>
+                       <td class="status ${cur.status}">${cur.status}</td>
                        <td><button class="delete">删除</button></td>
                      </tr>`;
     }, '');
@@ -69,22 +69,37 @@
           console.warn("不存在的状态")
       }
     });
-    $taskingNumber[0].innerHTML = data.length;
-  
+    $taskingNumber[0].innerHTML = data.length;  
     Array.from($taskingPercent).forEach((element, index) => element.innerHTML
-                                         = parseInt($taskingNumber[index + 1].innerHTML / $taskingNumber[0].innerHTML * 100) + "%");
+                                         = Math.round($taskingNumber[index + 1].innerHTML / $taskingNumber[0].innerHTML * 100) + "%");
   }
 
-  function deleteMask(id) {
+  function deleteMask() {
     document.getElementById("delete-mask").style.display = "block";
     document.getElementsByTagName("body")[0].className = "no-move";
-    deleteId = id; 
   }
 
   function deleteItem(id) {
     document.getElementById("delete-mask").style.display = "none";
-    // var $item = $projectTable.querySelector(`tr[data-id="${id}"]`);
-    // $projectTable.removeChild($projectTable.querySelector(`tr[data-id='${id}']`));
+    document.getElementsByTagName("body")[0].className = "";
+    var $item = $projectTable.querySelector(`tr[data-id="${id}"]`);
+    $projectTable.removeChild($item);
+    switch($item.children[3].innerHTML) {
+      case "ACTIVE":
+        $taskingNumber[1].innerHTML--; 
+        break;    
+      case "PENDING":
+        $taskingNumber[2].innerHTML--;
+        break;
+      case "CLOSED":
+        $taskingNumber[3].innerHTML--;
+        break;
+      default:
+        console.warn("不存在的状态")
+    }
+    $taskingNumber[0].innerHTML = $projectTable.children.length;
+    Array.from($taskingPercent).forEach((element, index) => element.innerHTML
+                                         = Math.round($taskingNumber[index + 1].innerHTML / $taskingNumber[0].innerHTML * 100) + "%");
   } 
   
   getListData();
@@ -92,7 +107,8 @@
     switch(e.target.className) {
       case "delete":
         let id = e.target.parentNode.parentNode.getAttribute('data-id');
-        deleteMask(id);
+        deleteMask();
+        deleteId = id;
         break;
       case "back iconfont":
       case "cancel":  
@@ -105,4 +121,4 @@
         console.log(e.target.className)  
     }
   })
-//})()
+})()
